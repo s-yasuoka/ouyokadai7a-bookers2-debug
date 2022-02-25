3de87792cd356eb49fb77a29ebb627a4ea383046
EC2 Default User <ec2-user@ip-172-31-20-123.ec2.internal>
2022-02-25 11:38:08 +0000

    [Uplo upload]

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+  get "search" => "searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]do
    resource :favorites, only:[:create,:destroy]
@@ -16,6 +17,7 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
